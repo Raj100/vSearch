73a0ab6c7189467b3dd02a61d596fd5e66442017
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2025-06-25 19:40:00 +0530

    added upload

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException
+from fastapi.staticfiles import StaticFiles
 
-app=FastAPI()
 
-@app.get("/hello/{name}")
-async def hello(name):
-    return f"wellcome to raj tutorial {name}"
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+qdrant = VectorDB(
+    url=os.getenv("QDRANT_URL", "http://localhost:6333"),
+    api_key=os.getenv("QDRANT_API_KEY", None),
+    collection_name=os.getenv("COLLECTION_NAME", "video_frames")
+)
+
+@app.post("/upload")
+async def upload_video(
+    file: UploadFile = File(...),
+    interval: int = Form(1, description="Frame extraction interval in seconds")
+):
+    try:
+        print("hello")
---
+++ models/database.py
@@ -0,0 +1,52 @@
+import os
+from qdrant_client import QdrantClient, models
+from dotenv import load_dotenv
+import uuid
+
+load_dotenv()
+
+class VectorDB:
+    def __init__(self, url, api_key, collection_name):
+        self.client = QdrantClient(
+            url=url,
+            api_key=api_key or None,
+            timeout=30
+        )
+        
+        self.collection_name = collection_name
+        self._create_collection()
+
+    def _create_collection(self):
+        try:
+            if not self.client.collection_exists(self.collection_name):
+                self.client.create_collection(
+                    collection_name=self.collection_name,
+                    vectors_config=models.VectorParams(
+                        size=256,
+                        distance=models.Distance.COSINE
+                    )
+                )
+        except Exception as e:
+            print(f"Collection creation failed: {str(e)}")
+            raise
+
+    def upsert(self, vector: list, payload: dict):
+        try:
+                frame_path = payload["frame_path"]
+                file_name = os.path.basename(frame_path)
+                point_id = str(uuid.uuid5(uuid.NAMESPACE_URL, file_name))
+                
+                self.client.upsert(
+                    collection_name=self.collection_name,
+                    points=[
+                        models.PointStruct(
+                            id=point_id,
+                            vector=vector,
+                            payload=payload
+                        )
+                    ]
+                )
+        except Exception as e:
+            print(f"Upsert failed: {str(e)}")
+            raise
+
